1bd01c2563146caf72723a4fcf7d2a088b15022c
박준현 <jun@GGo.local>
2020-08-07 11:33:31 +0900

    testing4

--- Test_JAVA/src/main/java/Starter.java
+++ Test_JAVA/src/main/java/Starter.java
@@ -5,7 +5,7 @@ import org.bson.Document;
 
 public class Starter {
     public static void main(String[] args){
-        MongoClient mongoClient = new MongoClient("54.180.117.70", 27017);
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
         MongoDatabase db = mongoClient.getDatabase("EQBR_core");
         MongoCollection<Document> documentMongoCollection = db.getCollection("known_hosts");
         print(documentMongoCollection.countDocuments());
